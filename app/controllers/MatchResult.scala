13843296181216d113982986539209ffef17416e
pabloalvareztoro <pabloalvareztoro@gmail.com>
2016-11-20 17:39:37 -0500

    Added handling for multiple matches

--- app/controllers/MatchResult.scala
+++ app/controllers/MatchResult.scala
@@ -2,20 +2,39 @@ package controllers
 
 import models.{Match, Team}
 import api.JsonCombinators._
-import play.api.libs.json.{JsResult, JsValue, Json}
+import play.api.libs.json.{JsArray, JsResult, JsValue, Json}
 import play.api.mvc._
+
+import scala.collection.mutable.ListBuffer
 
 object MatchResult extends Controller {
 
   def matchResult = Action(parse.json) { implicit request =>
     try{
-      val newMatch: Match = playMatch(request.body)
-      //Ok(newMatch.firstTeam.name + " " + newMatch.firstTeam.score + "-" + newMatch.secondTeam.score + " " + newMatch.secondTeam.name)
-      Ok(Json.toJson(newMatch))
+      request.body match{
+        case _: JsArray => {
+          val schedule: Seq[Match] = playSchedule(request.body.asInstanceOf[JsArray])
+          Ok(Json.toJson(schedule))
+        }
+        case _: JsValue => {
+          val newMatch: Match = playMatch(request.body)
+          Ok(Json.toJson(newMatch))
+        }
+      }
     }catch{
       case ex: NoSuchElementException => BadRequest
       case ex: Exception => InternalServerError
     }
+  }
+
+  def playSchedule(json: JsArray): Seq[Match] = {
+    val schedule: ListBuffer[Match] = ListBuffer[Match]()
+    val matches: Seq[JsValue] = json.value
+    for(x <- matches) {
+      val newMatch: Match = playMatch(x)
+      schedule.append(newMatch)
+    }
+    return schedule
   }
 
   def playMatch(json: JsValue): Match = {
